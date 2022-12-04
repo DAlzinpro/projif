e1417461eb5678215fe5ddffbf52cb1ccfcc6b76
DAlzinpro <101902234+DAlzinpro@users.noreply.github.com>
2022-12-04 20:27:28 -0300

    Create bem.js

---
+++ bem.js
@@ -0,0 +1,10 @@
+function sub() {
+    var check1 = document.getElementById("dif")
+    var check2 = document.getElementById("fac")
+
+    if (check2.checked == true){
+        window.open("https://dalzinpro.github.io/projif/jogo.html")
+    }else{
+        alert("nd")
+    }
+}

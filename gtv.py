6f7162134c1f7046568928b286bbe1a29eb6d712
DAlzinpro <101902234+DAlzinpro@users.noreply.github.com>
2023-04-26 22:21:02 -0300

    Update gtv.py
    B

--- gtv.py
+++ gtv.py
@@ -1,6 +1,6 @@
 import openai
 
 openai.api_key = "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"
-
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Me dê 3 ideias de aplicativos que eu posso criar com as api´s da openai"}])
+perg = input("")
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": perg}])
 print(completion.choices[0].message.content)

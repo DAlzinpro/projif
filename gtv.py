1bad8f0225a417f6f6ce7b0399946a16363041c3
DAlzinpro <101902234+DAlzinpro@users.noreply.github.com>
2023-04-26 22:27:24 -0300

    Update gtv.py
    V

--- gtv.py
+++ gtv.py
@@ -1,6 +1,7 @@
 import openai
 
 openai.api_key = "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"
+print("gay")
 perg = input("")
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": perg}])
 print(completion.choices[0].message.content)
